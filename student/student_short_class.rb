4e3ac7d4b39f25a87e56abd519f5fcc607b3047d
bigcodebingo <bachurin.ivan.04@mail.ru>
2024-10-19 01:20:39 +0300

    create short class

---
+++ student/student_short_class.rb
@@ -0,0 +1,33 @@
+require_relative 'student_class'
+
+class Student_short
+  attr_reader :id, :fio, :git, :contact
+
+  def initialize(student: nil, id: nil, info_str: nil)
+    if student
+      @id = student.id
+      @fio = student.full_name  
+      @git = student.github  
+      @contact = student.get_contacts
+    elsif id && info_str
+      @id = id
+      parsed_info = parse_info(info_str)
+      @fio = parsed_info[:fio]
+      @git = parsed_info[:git]
+      @contact = parsed_info[:contact]
+    end
+  end
+
+  private def parse_info(info_str)
+    parts = info_str.split(", ")
+    fio = parts[0]
+    git = parts[1].split("github: ")[1] if parts[1]
+    contact = parts[2].split("contacts: ")[1] if parts[2]
+    { fio: fio, git: git, contact: contact }
+  end
+
+  def to_s
+    "id: #{@id}, fullname: #{@fio}, git: #{@git || 'empty'}, contacts: #{@contact || 'empty'}"
+  end
+
+end
