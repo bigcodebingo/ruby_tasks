8b1191bb1e2463ff46c355dde4e19343a27620cd
bigcodebingo <bachurin.ivan.04@mail.ru>
2024-10-18 17:58:55 +0300

    ex.1 task.7

--- student/Student_class.rb
+++ student/Student_class.rb
@@ -1,24 +1,36 @@
 class Student
 
-    attr_reader :id, :name, :surname, :lastname
-    attr_accessor :phone, :telegram, :email, :github
-      
-    def initialize(id, name, surname, lastname, **extras)
-      @id = id
-      @name = name
-      @surname = surname
-      @lastname = lastname
-      @phone = extras[:phone]
-      @telegram = extras[:telegram]
-      @email = extras[:email]
-      @github = extras[:github]
+  attr_reader :id, :name, :surname, :lastname
+  attr_accessor :phone, :telegram, :email, :github
+    
+  def initialize(id, name, surname, lastname, **extras)
+    @id = id
+    @name = name
+    @surname = surname
+    @lastname = lastname
+    self.phone = extras[:phone]
+    @telegram = extras[:telegram]
+    @email = extras[:email]
+    @github = extras[:github]
+  end
+  
+  def self.valid_phone?(phone)
+    phone.match?(/^(?:\+7|8)\d{10}$/)
+  end
+  
+  def phone=(phone)
+    if phone.nil? || Student.valid_phone?(phone) 
+      @phone = phone
+    else 
+      raise ArgumentError, "неверный формат: #{phone}"
     end
-      
-      def full_name
-        "#{@surname} #{@name[0]}#{'.'}#{@lastname[0]}"
-      end
+  end
 
-      def to_s
-        "id: #{@id}, fullname: #{full_name}, phone: #{@phone || 'empty'}, tg: #{@telegram || 'empty'}, email: #{@email || 'empty'}, git: #{@github || 'empty'}"
-      end
+  def full_name
+    "#{@surname} #{@name[0]}#{'.'}#{@lastname[0]}"
+  end
+
+  def to_s
+    "id: #{@id}, fullname: #{full_name}, phone: #{@phone || 'empty'}, tg: #{@telegram || 'empty'}, email: #{@email || 'empty'}, git: #{@github || 'empty'}"
+  end
 end
