7d75b0e3a6ab688ee8e0908ecb34dcad75816e85
bigcodebingo <bachurin.ivan.04@mail.ru>
2024-10-18 18:04:06 +0300

    fix

--- student/main.rb
+++ student/main.rb
@@ -1,6 +1,6 @@
 require_relative 'Student_class'
 
-student1 = Student.new(1, "Ivan", "Ivanov", "Ivanovich", phone: "+79881677818", telegram: "@ivanov", email: "ivanov@example.com", github: "ivanov123")
+student1 = Student.new(1, "Ivan", "Ivanov", "Ivanovich", phone: "+79881677818", telegram: "@ivanovv", email: "ivanov@example.com", github: "ivanov123")
 student2 = Student.new(2, "Maria", "Petrova", "Sergeevna", phone: "89881677818", github: "mariapet")
 student3 = Student.new(3, "Sergey", "Sidorov", "Alexandrovich", telegram: "@sidorov")
 
