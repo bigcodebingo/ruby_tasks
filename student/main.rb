4852ebe3e11a582df64ca5e6a349b398a42e2567
bigcodebingo <bachurin.ivan.04@mail.ru>
2024-10-26 02:14:54 +0300

    (lab.2) fixes in main

--- student/main.rb
+++ student/main.rb
@@ -1,23 +1,28 @@
-require_relative 'student_class'
-require_relative 'student_short_class'
+require_relative 'student'
+require_relative 'student_short_class1'
 require_relative 'parent_student_class'
 
-student1 = Student.new(1, "Ivan", "Ivanov", "Ivanovich", phone: "+79881677818", telegram: "@ivanovv", email: "ivanov@example.com", github: "https://github.com/user-name_123")
-student2 = Student.new(2, "Maria", "Petrova", "Sergeevna", phone: "89881677818", github: "https://github.com/marry")
-student3 = Student.new(3, "Sergey", "Sidorov", "Alexandrovich",telegram: "@serjj", github: "https://github.com/serj")
+student1 = Student.new(id: 1, name: "Ivan", surname: "Ivanov", lastname: "Ivanovich", phone: "+79881677818", telegram: "@ivanovv", email: "ivanov@example.com", github: "https://github.com/user-name_123")
+student2 = Student.new(id: 2, name: "Maria", surname: "Petrova", lastname: "Sergeevna", phone: "89881677818", github: "https://github.com/marry")
+student3 = Student.new(id: 3, name: "Sergey", surname: "Sidorov", lastname: "Alexandrovich", telegram: "@serjj", github: "https://github.com/serj")
 
 puts student1
 puts student2
 puts student3
 puts ""
-student1.set_contacts(phone: '+79009998877', telegram: '@ivanov')
-puts student1
+# puts student1.getInfo
 puts ""
-puts student1.get_contacts
+student_short1 = Student_short.new(id: 1, fullname: "Бачурин И.А.", github: "https://github.com/ivan", contact: "+79881677818")
+# puts student_short1
 puts ""
-student_short1 = Student_short.new(student: student1)
-puts student_short1
+# puts student1.get_contacts
+puts ""
+student_short = Student_short.init_from_student(student1)
+puts ""
+# puts student_short
 info_str=student2.getInfo
-student_short2 = Student_short.new(id: 2, info_str: info_str)
+# puts ""
+# puts info_str
+student_short2 = Student_short.init_from_info(2, info_str)
 puts ""
 puts student_short2
