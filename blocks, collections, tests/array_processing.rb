25faeaac0091b774069179a323e9b6f6fccf0e6f
bigcodebingo <bachurin.ivan.04@mail.ru>
2024-11-29 23:06:10 +0300

    (lab.3) task.2 start

---
+++ blocks, collections, tests/array_processing.rb
@@ -0,0 +1,33 @@
+class Processing
+
+    attr_reader :array
+
+    def initialize(array)
+        @array = array
+    end
+
+    def count
+
+    end
+
+    def group_by
+
+    end
+
+    def partition
+
+    end
+
+    def take_while
+
+    end
+    
+    def min
+
+    end
+
+    def filter_map
+        
+    end
+
+end
