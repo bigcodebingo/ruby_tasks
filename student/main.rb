9abb1e9fcde9b72c69032dc28ed9545966eb62c2
bigcodebingo <bachurin.ivan.04@mail.ru>
2025-01-06 02:12:58 +0300

    change dir and some fixes

--- student/main.rb
+++ student/main.rb
@@ -1,7 +1,8 @@
 require_relative 'student'
 require_relative 'student_short'
-require_relative 'parent_student'
 require_relative './data_models/data_list_student_short.rb'
+require_relative 'student_list_yaml'
+require_relative 'student_list_json'
 
 # puts student1
 # puts student2
@@ -26,11 +27,12 @@ require_relative './data_models/data_list_student_short.rb'
 # puts names
 student1 = Student.new(id: 1, name: "Ivan", surname: "Ivanov", lastname: "Ivanovich", phone: "+79881677818", telegram: "@ivanovv", email: "ivanov@example.com", github: "https://github.com/user-name_123")
 student2 = Student.new(id: 2, name: "Maria", surname: "Petrova", lastname: "Sergeevna", phone: "89881677818", github: "https://github.com/marry")
-student3 = Student.new(id: 3, name: "Sergey", surname: "Sidorov", lastname: "Alexandrovich", telegram: "@serjj", github: "https://github.com/serj")
+# student3 = Student.new(id: 3, name: "Sergey", surname: "Sidorov", lastname: "Alexandrovich", telegram: "@serjj", github: "https://github.com/serj")
 
-st1_info = student1.getInfo
-st2_info = student2.getInfo
-st3_info = student3.getInfo
+# puts student1.full_name
+# st1_info = student1.getInfo
+# st2_info = student2.getInfo
+# st3_info = student3.getInfo
 
 # short_st1 = Student_short.init_from_info(1, st1_info)
 # short_st2 = Student_short.init_from_info(2, st2_info)
@@ -41,10 +43,23 @@ st3_info = student3.getInfo
 # puts short_st2
 # puts short_st3
 
-test = DataListStudentShort.new([
-    short_st1 = Student_short.init_from_info(1, st1_info),
-    short_st2 = Student_short.init_from_info(2, st2_info),
-    short_st3 = Student_short.init_from_info(3, st3_info)
-])
+# test = DataListStudentShort.new([
+#     short_st1 = Student_short.init_from_info(1, st1_info),
+#     short_st2 = Student_short.init_from_info(2, st2_info),
+#     short_st3 = Student_short.init_from_info(3, st3_info)
+# ])
 
-puts test.get_data()
+# puts test.get_data()
+students_list = StudentsListJSON.new('students.json')
+# students_list.delete_student_by_id(1)
+puts students_list.students
+new_student = Student.new(id: 5, name: "Sergey", surname: "Sidorov", lastname: "Alexandrovich", telegram: "@serjj", github: "https://github.com/serj")
+students_list.add_student(new_student)
+
+puts 
+# puts students_list.sort_by_surname_fullname!
+
+students_list.save_to_file
+
+# data_list = students_list.get_k_n_student_short_list(1, 3)
+# puts data_list.get_data()
