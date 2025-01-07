bbe52f01b3f81575b9ea5896e7e088fbdbb2ab9a
bigcodebingo <bachurin.ivan.04@mail.ru>
2025-01-08 01:25:03 +0300

    (lab.4) test

--- student/main.rb
+++ student/main.rb
@@ -1,9 +1,10 @@
 require_relative 'student'
-# require_relative 'student_short'
+require_relative 'student_short'
 require_relative './data_models/data_list_student_short.rb'
 require_relative './strategy/student_list_yaml'
 require_relative './strategy/student_list_json'
 require_relative './strategy/student_list'
+require_relative './database/student_list_db'
 # puts student1
 # puts student2
 # puts student3
@@ -27,16 +28,16 @@ require_relative './strategy/student_list'
 # puts names
 student1 = Student.new(id: 1, name: "Ivan", surname: "Ivanov", lastname: "Ivanovich", phone: "+79881677818", telegram: "@ivanovv", email: "ivanov@example.com", github: "https://github.com/user-name_123")
 student2 = Student.new(id: 2, name: "Maria", surname: "Petrova", lastname: "Sergeevna", phone: "89881677818", github: "https://github.com/marry")
-# student3 = Student.new(id: 3, name: "Sergey", surname: "Sidorov", lastname: "Alexandrovich", telegram: "@serjj", github: "https://github.com/serj")
+student3 = Student.new(id: 3, name: "Sergey", surname: "Sidorov", lastname: "Alexandrovich", telegram: "@serjj", github: "https://github.com/serj")
 
 # puts student1.full_name
-# st1_info = student1.getInfo
-# st2_info = student2.getInfo
-# st3_info = student3.getInfo
+st1_info = student1.getInfo
+st2_info = student2.getInfo
+st3_info = student3.getInfo
 
-# short_st1 = Student_short.init_from_info(1, st1_info)
-# short_st2 = Student_short.init_from_info(2, st2_info)
-# short_st3 = Student_short.init_from_info(3, st3_info)
+short_st1 = Student_short.init_from_info(1, st1_info)
+short_st2 = Student_short.init_from_info(2, st2_info)
+short_st3 = Student_short.init_from_info(3, st3_info)
 
 # puts short_st1.get_contact
 # puts short_st1
@@ -49,7 +50,8 @@ student2 = Student.new(id: 2, name: "Maria", surname: "Petrova", lastname: "Serg
 #     short_st3 = Student_short.init_from_info(3, st3_info)
 # ])
 
-# puts test.get_data()
+
+# puts test.select(0)
 # students_list = StudentsListJSON.new('students.json')
 # students_list.delete_student_by_id(1)
 # puts students_list.students
@@ -63,14 +65,19 @@ student2 = Student.new(id: 2, name: "Maria", surname: "Petrova", lastname: "Serg
 # data_list = students_list.get_k_n_student_short_list(1, 3)
 # puts data_list.get_data()
 
-json_strategy = StudentsListJSON.new
-students_list = StudentsList.new('students.json', json_strategy)
+# json_strategy = StudentsListJSON.new
+# students_list = StudentsList.new('students.json', json_strategy)
 
 
-students_list.add_student(Student.new(id: nil, name: 'Иван', surname: 'Иванович', lastname: 'Иванов'))
+# students_list.add_student(Student.new(id: nil, name: 'Иван', surname: 'Иванович', lastname: 'Иванов'))
 
-yaml_strategy = StudentsListYAML.new
-students_list.update_strategy('students.yaml', yaml_strategy)
-students_list.add_student(Student.new(id: nil, name: 'Иваныч', surname: 'Иванович', lastname: 'сосал'))
+# yaml_strategy = StudentsListYAML.new
+# students_list.update_strategy('students.yaml', yaml_strategy)
+# students_list.add_student(Student.new(id: nil, name: 'Иваныч', surname: 'Иванович', lastname: 'сосал'))
 
-students_list.save_students
+# students_list.save_students
+
+student_db = StudentsListDB.new()
+# puts student_db.get_student_by_id(11)
+
+puts student_db.get_student_short_count
