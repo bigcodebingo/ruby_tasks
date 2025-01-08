b1e3893866e7af87cb78878e6678347560b817a5
bigcodebingo <bachurin.ivan.04@mail.ru>
2025-01-08 12:35:58 +0300

    (lab.4) add any?

--- student/strategy/student_list.rb
+++ student/strategy/student_list.rb
@@ -7,11 +7,11 @@ class StudentsList
         @students = load_students
     end
 
-    def load_students
+    def read_from_file
         @storage_strategy.read_from_file(@file_path)
     end
 
-    def save_students
+    def save_to_file
         to_data_hash = @students.map(&:to_h)
         @storage_strategy.save_to_file(@file_path, to_data_hash)
     end
@@ -44,24 +44,28 @@ class StudentsList
     end
 
     def add_student(student)
+        if @students.any? { |element| element == student }
+            raise NameError, 'уже существует'
+        end
         student_id_list = @students.map(&:id)
         max_id = student_id_list.max || 0
         student.id = max_id + 1
         @students << student
-        save_students
     end
 
     def update_student_by_id(id, new_student)
         index = @students.find_index { |student| student.id == id }
         raise IndexError, "wrong id" unless index
+        if @students.any? { |element| element == new_student }
+            raise NameError, 'уже существует'
+        end
         @students[index] = new_student
         new_student.id = id
-        save_students
     end
 
     def delete_student_by_id(id)
         @students.reject! { |student| student.id == id }
-        save_students
+        save_to_file
     end
 
     def get_student_short_count
