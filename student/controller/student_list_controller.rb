6451bd45017588455b5334cd008a4194f3612127
bigcodebingo <bachurin.ivan.04@mail.ru>
2025-01-24 04:05:21 +0300

    (lab.5) task.1.3 add controller

---
+++ student/controller/student_list_controller.rb
@@ -0,0 +1,46 @@
+require './data_models/data_list_student_short.rb'
+require './strategy/student_list.rb'
+
+class StudentListController
+
+    def initialize(view)
+        @view = view
+        @student_list = StudentsList.new("./students.json", StudentsListJSON.new)
+        @data_list = DataListStudentShort.new([])
+		@data_list.add_observer(@view)
+    end
+
+    def sort_by_fullname!
+        @student_list.sort_by_fullname!
+    end
+
+    def refresh_data
+        total_students = @student_list.get_student_short_count
+        total_pages = (total_students.to_f / @view.items_per_page).ceil
+        @student_list.get_k_n_student_short_list(@view.current_page, @view.items_per_page, @data_list)
+        @data_list.count = total_students
+        @data_list.notify
+        update_pagination(@view.current_page, total_pages) 
+      end
+    
+    def update_pagination(current_page, total_pages)
+        @view.page_label.text = "Страница: #{current_page}/#{total_pages}"
+    end
+
+    def change_page(page)
+        total_students = @student_list.get_student_short_count
+        total_pages = (total_students.to_f / @view.items_per_page).ceil
+
+        new_page = @view.current_page + page
+        return if new_page < 1 || new_page > total_pages
+
+        @view.current_page = new_page
+        refresh_data
+        update_pagination(@view.current_page, total_pages)
+    end
+
+    def update()
+		refresh_data
+	end
+
+end
--- student/view/student_list_view.rb
+++ student/view/student_list_view.rb
@@ -1,17 +1,23 @@
+require './controller/student_list_controller.rb'
+require './strategy/student_list.rb'
 require 'fox16'
 include Fox
 
 class StudentApp < FXMainWindow
-  def initialize(app)
-    super(app, "Students", width: 920, height: 600)
 
-    @student_db = StudentsListDB.new()
+  attr_reader :items_per_page, :page_label
+  attr_accessor :current_page
+  
+  def initialize(app)
+    super(app, "students", width: 920, height: 600)
+
+    @controller = StudentListController.new(self)
     @current_page = 1
-    @items_per_page = 20
+    @items_per_page = 15
 
     tab_book = FXTabBook.new(self, nil, 0, LAYOUT_FILL_X | LAYOUT_FILL_Y)
-    tab1 = FXTabItem.new(tab_book, "Student List View", nil)
-    tab1_frame = FXHorizontalFrame.new(tab_book, LAYOUT_FILL_X | LAYOUT_FILL_Y)
+    tab1 = FXTabItem.new(tab_book, "1", nil)
+    @tab1_frame = FXHorizontalFrame.new(tab_book, LAYOUT_FILL_X | LAYOUT_FILL_Y)
 
     tab2 = FXTabItem.new(tab_book, "2")
     FXVerticalFrame.new(tab_book, LAYOUT_FILL).tap do |frame|
@@ -22,41 +28,77 @@ class StudentApp < FXMainWindow
       FXLabel.new(frame, nil, nil, LAYOUT_CENTER_X)
     end
 
-    filter_frame = FXVerticalFrame.new(tab1_frame, FRAME_SUNKEN | LAYOUT_FILL_Y | LAYOUT_FIX_WIDTH, width: 170)
+    filter_frame = FXVerticalFrame.new(@tab1_frame, FRAME_SUNKEN | LAYOUT_FILL_Y | LAYOUT_FIX_WIDTH, width: 170)
     FXLabel.new(filter_frame, "ФИО:")
     @name_field = FXTextField.new(filter_frame, 25, nil, 0, TEXTFIELD_NORMAL | LAYOUT_FILL_X)
-    create_filter_section(filter_frame, "github:")
-    create_filter_section(filter_frame, "email:")
-    create_filter_section(filter_frame, "phone:")
-    create_filter_section(filter_frame, "telegram:")
 
-    table_frame = FXVerticalFrame.new(tab1_frame, FRAME_SUNKEN | LAYOUT_FILL_X | LAYOUT_FILL_Y)
-    @student_table = FXTable.new(table_frame, nil, 0, TABLE_COL_SIZABLE | LAYOUT_FILL | TABLE_READONLY | TABLE_NO_COLSELECT)
-    controls = FXHorizontalFrame.new(table_frame, opts:LAYOUT_CENTER_X)
-    prev_btn = FXButton.new(controls, "пред.")
-    @page_label = FXLabel.new(controls, "стр: 1/1", nil, JUSTIFY_CENTER_X)
-    next_btn = FXButton.new(controls, "след.")
-    prev_btn.connect(SEL_COMMAND) { change_page(-1) }
-    next_btn.connect(SEL_COMMAND) { change_page(1) }
-
-    
-    control_frame = FXVerticalFrame.new(tab1_frame, FRAME_SUNKEN | LAYOUT_FILL_Y | LAYOUT_FIX_WIDTH, width: 100)
-    @add_btn = FXButton.new(control_frame, "Добавить", nil, nil, 0, BUTTON_NORMAL | LAYOUT_FILL_X)
-    @edit_btn =FXButton.new(control_frame, "Изменить", nil, nil, 0, BUTTON_NORMAL | LAYOUT_FILL_X)
-    @delete_btn = FXButton.new(control_frame, "Удалить", nil, nil, 0, BUTTON_NORMAL | LAYOUT_FILL_X)
-    @update_btn = FXButton.new(control_frame, "Обновить", nil, nil, 0, BUTTON_NORMAL | LAYOUT_FILL_X)
-    @student_table.connect(SEL_SELECTED) { update_button_states }
-    @student_table.connect(SEL_DESELECTED) { update_button_states }
+    create_filter_frame(filter_frame, "github:")
+    create_filter_frame(filter_frame, "email:")
+    create_filter_frame(filter_frame, "phone:")
+    create_filter_frame(filter_frame, "telegram:")
+    create_table_frame
+    create_buttons_frame
     update_button_states
 
     self.connect(SEL_CLOSE) do
       app.exit
     end 
-
-    load_from_db  
+    
+    @controller.refresh_data  
   end
 
-  def get_selected_rows()
+  def create_buttons_frame
+    button_frame = FXVerticalFrame.new(@tab1_frame, FRAME_SUNKEN | LAYOUT_FILL_Y | LAYOUT_FIX_WIDTH, width: 100)
+    @add_btn = FXButton.new(button_frame, "Добавить", nil, nil, 0, BUTTON_NORMAL | LAYOUT_FILL_X)
+    @edit_btn =FXButton.new(button_frame, "Изменить", nil, nil, 0, BUTTON_NORMAL | LAYOUT_FILL_X)
+    @delete_btn = FXButton.new(button_frame, "Удалить", nil, nil, 0, BUTTON_NORMAL | LAYOUT_FILL_X)
+    @update_btn = FXButton.new(button_frame, "Обновить", nil, nil, 0, BUTTON_NORMAL | LAYOUT_FILL_X)
+    @update_btn.connect(SEL_COMMAND) { on_update }
+  end
+
+  def create_table_frame
+    table_frame = FXVerticalFrame.new(@tab1_frame, FRAME_SUNKEN | LAYOUT_FILL_X | LAYOUT_FILL_Y)
+    @student_table = FXTable.new(table_frame, nil, 0, TABLE_COL_SIZABLE | LAYOUT_FILL | TABLE_READONLY)
+    controls = FXHorizontalFrame.new(table_frame, opts:LAYOUT_CENTER_X)
+    prev_btn = FXButton.new(controls, "<")
+    @page_label = FXLabel.new(controls, "страница: 1/1", nil, JUSTIFY_CENTER_X)
+    next_btn = FXButton.new(controls, ">")
+    prev_btn.connect(SEL_COMMAND) { change_page(-1) }
+    next_btn.connect(SEL_COMMAND) { change_page(1) }
+    @student_table.connect(SEL_SELECTED) { update_button_states }
+    @student_table.connect(SEL_DESELECTED) { update_button_states }
+    @student_table.columnHeader.connect(SEL_COMMAND) do |_, _, sel|
+      column_index = sel
+      if column_index == 1
+        @controller.sort_by_fullname!
+      end
+      @controller.refresh_data 
+    end 
+  end
+
+  def create_filter_frame(frame, label_text)
+    FXLabel.new(frame, label_text)
+    combo = FXComboBox.new(frame, 3, nil, 0, COMBOBOX_STATIC | LAYOUT_FILL_X)
+    combo.numVisible = 3
+    combo.appendItem("да")
+    combo.appendItem("нет")
+    combo.appendItem("не важно")
+    combo.setCurrentItem(2)
+
+    input_field = FXTextField.new(frame, 25, nil, 0, TEXTFIELD_NORMAL | LAYOUT_FILL_X)
+    input_field.enabled = false
+
+    combo.connect(SEL_COMMAND) do
+      if combo.currentItem == 0
+        input_field.enabled = true
+      else
+        input_field.enabled = false
+        input_field.text = "" 
+      end
+    end
+  end
+
+  def get_selected_rows
     selected_rows = []
     (0...@student_table.numRows).each do |row|
         selected_rows << row if @student_table.rowSelected?(row)
@@ -64,7 +106,7 @@ class StudentApp < FXMainWindow
     selected_rows
   end
 
-  def update_button_states()
+  def update_button_states
       selected_rows = get_selected_rows
       case selected_rows.size
       when 0
@@ -79,71 +121,51 @@ class StudentApp < FXMainWindow
       end
   end
 
-  def create_filter_section(frame, label_text)
-    section_frame = FXVerticalFrame.new(frame, FRAME_SUNKEN | LAYOUT_FILL_X)
-    FXLabel.new(section_frame, label_text)
-    combo = FXComboBox.new(section_frame, 3, nil, 0, COMBOBOX_STATIC | LAYOUT_FILL_X)
-    combo.numVisible = 3
-    combo.appendItem("да")
-    combo.appendItem("нет")
-    combo.appendItem("не важно")
-    combo.setCurrentItem(2)
-
-    input_field = FXTextField.new(section_frame, 25, nil, 0, TEXTFIELD_NORMAL | LAYOUT_FILL_X)
-    input_field.enabled = false
-
-    combo.connect(SEL_COMMAND) do
-      if combo.currentItem == 0
-        input_field.enabled = true
-      else
-        input_field.enabled = false
-        input_field.text = "" 
-      end
-    end
+  def on_update()
+    @controller.update
   end
 
-  private def set_column_widths
+  def set_column_widths
     @student_table.setColumnWidth(0, 50)  
     @student_table.setColumnWidth(1, 100) 
     @student_table.setColumnWidth(2, 235) 
     @student_table.setColumnWidth(3, 130) 
   end
 
-  private def load_from_db
-    total_students = @student_db.get_student_short_count
-    total_pages = (total_students.to_f / @items_per_page).ceil
-    @page_label.text = "стр: #{@current_page}/#{total_pages}"
-
-    data_list = DataListStudentShort.new(@student_db.get_k_n_student_short_list(@current_page, @items_per_page))
-    data_table = data_list.get_data
-    column_names = ["ID", "ФИО", "Контакт", "Github"]
-    @student_table.setTableSize(data_table.row_count, data_table.column_count)
-
+  def set_table_params(column_names, whole_entities_count)
+    @page_label.text = "страница: #{@current_page}/#{whole_entities_count}"
+    @student_table.setTableSize(15, 4)
     column_names.each_with_index do |name, index|
       @student_table.setColumnText(index, name) 
     end
+  end
+
+  def set_table_data(data_table)
+    offset = (@current_page - 1) * @items_per_page
 
     (0...data_table.row_count).each do |row_index|
       (0...data_table.column_count).each do |col_index|
         value = data_table.get_element(row_index, col_index).to_s
+        value = (row_index + 1 + offset).to_s if col_index == 0
         @student_table.setItemText(row_index, col_index, value)
       end
     end
-    
     set_column_widths
-  end
-
-  def change_page(page)
-    total_students = @student_db.get_student_short_count
-    total_pages = (total_students.to_f / @items_per_page).ceil
-    new_page = @current_page + page
-    return if new_page < 1 || new_page > total_pages
-    @current_page = new_page
-    load_from_db
   end
 
   def create
     super
     show(PLACEMENT_SCREEN)
   end
+
+  private
+
+  def update_pagination(current_page, total_pages)
+    @controller.update_pagination(current_page, total_pages)
+  end
+
+  def change_page(page)
+    @controller.change_page(page)
+  end
+
 end
